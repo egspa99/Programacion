c969379d767fe94e5cfe955b1e38c6474f3b90f0
egspa99 <0337egarcia@e-itaca.es>
2024-04-25 09:19:23 +0200

    update

---
+++ src/Ejercicios/Camion.java
@@ -0,0 +1,8 @@
+package Ejercicios;
+
+public class Camion {
+
+	String matricula;
+	double velocidad;
+	boolean remolque = false;
+}
---
+++ src/Ejercicios/Vehiculos.java
@@ -0,0 +1,17 @@
+package Ejercicios;
+
+public class Vehiculos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Coche coche = new Coche();
+		
+		coche.matricula="2356HHH";
+		coche.puertas=4;
+		coche.velocidad=60;
+		
+		System.out.println(coche.toString());
+	}
+
+}
